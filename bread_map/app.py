7eae358e1b97be6b79c6442a1a4aa5607b7efbe0
yeongkyung <64993122+yeongkyung@users.noreply.github.com>
2020-06-13 17:56:59 +0900

    최종 프로젝트
    지도에 여러 마커 표시하기

--- bread_map/app.py
+++ bread_map/app.py
@@ -6,18 +6,14 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
-
-# HTML을 주는 부분
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/api/list', methods=['GET'])
+@app.route('/places', methods=['GET'])
 def bakery_list():
-    # 1. bakery 목록 전체 검색
-    bakery = list(db.bakery.find({},{'_id':False})
-    # 2. 성공하면 success 메시지와 함께 bakery_list 목록을 클라이언트에 전달합니다.
-    return jsonify({'result': 'success','bakery_list':bakery})
+    bakerys = list(db.bakery.find({},{'_id':False}))
+    return jsonify({'result': 'success','bakerys_list': bakerys})
 
 
 if __name__ == '__main__':
