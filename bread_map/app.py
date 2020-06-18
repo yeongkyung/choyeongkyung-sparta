ffdeb8f324c3decf66f1d10d2a651f5a3ec603ac
yeongkyung <64993122+yeongkyung@users.noreply.github.com>
2020-06-19 07:35:45 +0900

    필터 반영

--- bread_map/app.py
+++ bread_map/app.py
@@ -1,25 +1,35 @@
-from pymongo import MongoClient
-
 from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+import json
+import requests
+from bs4 import BeautifulSoup
+
+def remove_dupe_dicts(l):
+    return [dict(t) for t in {tuple(d.items()) for d in l}]
+
+
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/places', methods=['GET'])
-def bakery_list():  
+def bakery_list():
     filter = request.args.get('filter')
     filter_list = filter.split(',')
-    bakerys=[]
+    bakerys = []
     for category in filter_list:
-        bakery = list(db.bakery.find({category:True},{'_id':False}))
+        bakery = list(db.bakery.find({category: True}, {'_id': False}))
         bakerys.extend(bakery)
-    # 리스트에 중복된 딕셔너리 제거하는 방법 찾아야 함
-    return jsonify({'result': 'success','bakerys_list': bakerys})
+    # 리스트에 중복된 딕셔너리 제거
+    bakerys_list = remove_dupe_dicts(bakerys)
+    return jsonify({'result': 'success', 'bakerys_list': bakerys_list})
 
 
 if __name__ == '__main__':
